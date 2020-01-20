0c4a2ba42f002472dec7295acc971b631c864667
Mario Fornaroli <mario.fornaroli@sysdata.it>
2020-01-20 16:07:01 +0100

    General items - custom classes and styles

--- src/components/texts/paragraph/index.js
+++ src/components/texts/paragraph/index.js
@@ -5,10 +5,12 @@ import style from './style.css'
 class Paragraph extends Component {
 
     render(){
-        var {id,i, content} = this.props;
+        var { id, i, content, styleObj } = this.props;
+        const customClasses = this.props.classes ? ' ' + this.props.classes : '';
         return (
             <div key={id+"_"+i} 
-            className={style.paragraph} 
+            className={style.paragraph + customClasses}
+            style={styleObj}
             onClick={this.props.onClick} >{content}</div>
         )
     }
